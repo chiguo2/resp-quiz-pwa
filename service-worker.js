e3b7fdf1b9e39e409a72da25778dcb1d4435d668
chiguo2 <rockboxexp2000@gmail.com>
2026-06-17 11:33:56 +0900

    問題と参照ページ追加し重複削除

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'resp-quiz-cache-v10-reference-navigation-qadd1';
+const CACHE_NAME = 'resp-quiz-cache-v11-question-content-dedupe';
 const CORE_ASSETS = [
   './','./index.html','./style.css','./app.js','./manifest.json','./icon.svg',
   './reference-viewer.html','./reference-viewer.css','./reference-viewer.js',
